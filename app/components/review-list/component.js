9d853da532595d98000e4a90c121e681f2b0490d
April Peng and Mark Lawson <april+mark@>
2016-11-03 16:06:17 -0700

    sort reviews with select input

--- app/components/review-list/component.js
+++ app/components/review-list/component.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  sortBy: ['rating'],
+  sortBy: ['rating:desc'],
   sortedReviews: Ember.computed.sort('item.reviews', 'sortBy'),
+  actions: {
+    updateSort() {
+      var value = this.$('#sortReviews').val();
+      this.set('sortBy', [value]);
+    }
+  }
 });
